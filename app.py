4b803e66ee6751aed1a8bd0fb010a7b15150896a
khummel <hummka01@luther.edu>
2018-04-21 10:15:06 -0500

    macro and favorites

--- app.py
+++ app.py
@@ -13,8 +13,19 @@ def recipes():
     ingredients = request.form['ingredients']
     ingredients = ingredients.replace(' ','%20')
     res = requests.get("https://api.edamam.com/search?q="+ingredients+"&app_id=7f091258&app_key=c3905a1267e3c6a8fa220231cb84004e&from=0&to=3")
-    print(res.json())
+    # print(res.json())
     return render_template('recipes.html', res=res.json()['hits'])
+
+@app.route('/macro', methods=['POST'])
+def macro():
+    pass
+
+@app.route('/favorite', methods=['POST'])
+def saveFav():
+    newFav = request.form(['addingFavorite'])
+
+
+    return render_template('favorites.html', res=res.json()#####)
 
 
 if __name__=='__main__':
